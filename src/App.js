e4ab135529cb13a7689783262723745dc8e76e1c
Ahsan <ahsan.161815@gmail.com>
2023-10-12 10:33:45 +0500

    components and props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+
+const Person = (props) => {
+  return (
+    <>
+    <p> FirstName :{props.firstName}  LastName: {props.lastName} </p>
+    </>
+  );
+}
+
+const App = () => {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Person firstName="Ahsan" lastName="Khan" />
+      <Person firstName="Ali" lastName="la" />
+      <Person firstName="Ahmad" lastName="elahi" />
     </div>
   );
 }
