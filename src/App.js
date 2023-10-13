b8474ca94c43be5f2a74c7e3f2465bbb401f64a0
Ahsan <ahsan.161815@gmail.com>
2023-10-13 18:43:25 +0500

    changes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
 import './App.css';
-
-
-const Person = (props) => {
-  return (
-    <>
-    <p> FirstName :{props.firstName}  LastName: {props.lastName} </p>
-    </>
-  );
-}
+import { useState, useEffect } from 'react';
 
 const App = () => {
+  const [count, setCount] = useState(0);
+
+  useEffect(() => {
+    setCount(1000);
+  }, []);
+
   return (
     <div className="App">
-      <Person firstName="Ahsan" lastName="Khan" />
-      <Person firstName="Ali" lastName="la" />
-      <Person firstName="Ahmad" lastName="elahi" />
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <h1>{count}</h1>
+      <button onClick={() => setCount(count + 1)}>+</button>
     </div>
   );
 }
